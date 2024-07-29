1aa8ac3fff460eabd6de071ea92df384acde17b5
M. Buğra Kurnaz <muammerkurnaz1071@gmail.com>
2024-07-29 22:41:13 +0300

    Initial page layout - not fully working yet

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="uk-grid uk-grid-collapse">
+    <NavigationBar />
+    <div class="uk-width-expand uk-padding" uk-height-viewport>
+      <h1>Main Content</h1>
+      <p>This is the main content area.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
+
+import NavigationBar from './components/NavigationBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar,
+  },
+};
+</script>
+
+<style lang="less">
+    @import "../node_modules/uikit/src/less/uikit.less";
+</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="uk-offcanvas-content">
+    <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #offcanvas-nav-primary">
+      Open Navigation
+    </button>
+
+    <div id="offcanvas-nav-primary" uk-offcanvas="mode: reveal; overlay: true">
+      <div class="uk-offcanvas-bar uk-flex uk-flex-column">
+
+        <button class="uk-offcanvas-close" type="button" uk-close></button>
+
+        <ul class="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
+          <li class="uk-active"><a href="#">Active</a></li>
+          <li class="uk-parent">
+            <a href="#">Parent</a>
+            <ul class="uk-nav-sub">
+              <li><a href="#">Sub item</a></li>
+              <li><a href="#">Sub item</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Item</a></li>
+          <li><a href="#">Item</a></li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar',
+};
+</script>
+
+<style scoped>
+.uk-offcanvas-bar {
+  width: 250px;
+}
+</style>
